8f3411114b4d32edb23df5b70f31920ebb6bbc5d
Johanna <lfarfan21@uan.edu.co>
2020-10-21 23:35:42 -0500

    5 commit

--- PruebaHeroku/settings.py
+++ PruebaHeroku/settings.py
@@ -46,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PruebaHeroku.urls'
@@ -121,4 +123,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
