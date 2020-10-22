1d33f1a5bc275666e4a685953829bf47cc71d9de
Johanna <lfarfan21@uan.edu.co>
2020-10-21 23:44:15 -0500

    6 commit

--- PruebaHeroku/wsgi.py
+++ PruebaHeroku/wsgi.py
@@ -7,19 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from whitenoise.django import DjangoWhiteNoise
-
-from PruebaHeroku import MyWSGIApp
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PruebaHeroku.settings')
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
-application = MyWSGIApp()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise import WhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
+#
+# from PruebaHeroku import MyWSGIApp
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PruebaHeroku.settings')
+#
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
+#
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root='/path/to/static/files')
+# application.add_files('/path/to/more/static/files', prefix='more-files/')
